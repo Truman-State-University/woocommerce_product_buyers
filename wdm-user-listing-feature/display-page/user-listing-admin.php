4a8d7735af72100f77da2c261b2a83f86ebec06e
Greg Marshall <gmarsh@truman.edu>
2016-07-06 14:36:09 -0500

    Fixed bug with empty settings. Fixed product filter when product name has an apostrophe. Added Quantities column

--- wdm-user-listing-feature/display-page/user-listing-admin.php
+++ wdm-user-listing-feature/display-page/user-listing-admin.php
@@ -1,7 +1,7 @@
 <?php
 
 global $woocommerce;
-$wc_fields_additional=get_option('wc_fields_additional','no value');
+$wc_fields_additional=get_option('wc_fields_additional',array());
 
 $all_products_name_array=get_all_products_name();
 $all_categories_name_array=get_all_categories_name();
@@ -27,144 +27,149 @@ wp_localize_script( 'wdm_custom_js', 'wdm_all_products_name', $all_products_name
 
 
 ?>
-<div id='wdm-user-listing-div'>
-<h1>Product Buyers</h1>
+	<div id='wdm-user-listing-div'>
+		<h1>Product Buyers</h1>
 
-<div>
-Hide column: <br>
-<input type="checkbox" class="toggle-vis" data-column="0"> Order Id <br>
-<input type="checkbox" class="toggle-vis" data-column="1"> Date <br>
-<input type="checkbox" class="toggle-vis" data-column="2"> Time <br>
-<input type="checkbox" class="toggle-vis" data-column="3"> Purchaser <br>
-<?php
-		$column_id=4;
-        foreach($wc_fields_additional as $key=>$values)
-        {
-           echo '<input type="checkbox" class="toggle-vis" data-column="'.$column_id.'"> '.$values['label'].' <br>';
-			$column_id++;
-        }
-                ?>
-				<input type="checkbox" class="toggle-vis" data-column="<?php echo $column_id; ?>"> Products <br>
-				</div>
-<br>
-<table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
-	    <thead>
-            <tr class='filter-row'>
-				<th style="border: 0px"></th>
-                <th style="border: 0px"></th>
+		<div>
+			Hide column: <br>
+			<input type="checkbox" class="toggle-vis" data-column="0"> Order Id <br>
+			<input type="checkbox" class="toggle-vis" data-column="1"> Date <br>
+			<input type="checkbox" class="toggle-vis" data-column="2"> Time <br>
+			<input type="checkbox" class="toggle-vis" data-column="3"> Purchaser <br>
+			<?php
+			$column_id=4;
+			foreach($wc_fields_additional as $key=>$values)
+			{
+				echo '<input type="checkbox" class="toggle-vis" data-column="'.$column_id.'"> '.$values['label'].' <br>';
+				$column_id++;
+			}
+			?>
+			<input type="checkbox" class="toggle-vis" data-column="<?php echo $column_id; ?>"> Products <br>
+		</div>
+		<br>
+		<table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
+			<thead>
+			<tr class='filter-row'>
 				<th style="border: 0px"></th>
 				<th style="border: 0px"></th>
-                <?php
-                    foreach($wc_fields_additional as $key=>$values)
-                    {
-                        echo '<th style="border: 0px"></th>';
-                    }
-                ?>
+				<th style="border: 0px"></th>
+				<th style="border: 0px"></th>
+				<?php
+				foreach($wc_fields_additional as $key=>$values)
+				{
+					echo '<th style="border: 0px"></th>';
+				}
+				?>
 				<th style="border: 0px"></th>
 			</tr>
 			<tr>
-                <th data-pos='1'>Order Id</th>
-                <th data-pos='2'>Date</th>
+				<th data-pos='1'>Order Id</th>
+				<th data-pos='2'>Date</th>
 				<th data-pos='3'>Time</th>
 				<th data-pos='3'>Purchaser</th>
-                <?php
+				<?php
 				$column_id=4;
-                    foreach($wc_fields_additional as $key=>$values)
-                    {
-                        echo '<th data-pos='.$column_id.'>'.$values['label'].'</th>';
-						$column_id++;
-                    }
-                ?>
+				foreach($wc_fields_additional as $key=>$values)
+				{
+					echo '<th data-pos='.$column_id.'>'.$values['label'].'</th>';
+					$column_id++;
+				}
+				?>
 				<th data-pos='<?php echo $column_id; ?>'>Products</th>
-            </tr>
-        </thead>
-		<tbody>
-				
-        <?php
-				global $wp_query;
-				$args = array(
-						'post_type' => 'shop_order',
-						'post_status' => array('wc-pending','wc-processing','wc-completed'),
-						'posts_per_page' => -1,
-				);
+				<th data-pos='<?php echo $column_id; ?>'>Quantities</th>
+			</tr>
+			</thead>
+			<tbody>
 
-				// The Query
-				$wp_query=new WP_Query( $args );
+			<?php
+			global $wp_query;
+			$args = array(
+				'post_type' => 'shop_order',
+				'post_status' => array('wc-pending','wc-processing','wc-completed'),
+				'posts_per_page' => -1,
+			);
 
-				// The Loop
-				while ( have_posts() ) : the_post();
-						echo '<tr>';
-						echo '<td>'.get_the_ID().'</td>';
-						echo '<td>'.get_the_date('d/m/Y').'</td>';
-						echo '<td>'.get_the_time().'</td>';
-						echo '<td>'.get_post_meta( get_the_ID(),'_billing_first_name', true ).' '.
-							    get_post_meta( get_the_ID(),'_billing_last_name', true ).' &#60;'.
-							    get_post_meta( get_the_ID(),'_billing_email', true ).'&#62;</td>';
-						foreach($wc_fields_additional as $key=>$values)
-						{
-								echo '<td>'.get_post_meta( get_the_ID(),$key, true ).'</td>';
-						}
-	
-						$order_id=get_the_ID();
-						$order = new WC_Order( $order_id );
-						$items = $order->get_items();
-		
-						$order_items=array();
-						foreach ( $items as $item ) {
-							$product_name = $item['name'];
-						    $product_id = $item['product_id'];
-						    $product_variation_id = $item['variation_id'];
-							array_push($order_items,$product_name);
-						}
-						echo '<td>'.implode(' , ',$order_items).'</td>';
-						echo '</tr>';
-				endwhile;
+			// The Query
+			$wp_query=new WP_Query( $args );
 
-				// Reset Query
-				wp_reset_query();
-		?>
-		</tbody>
-</table>
-<div>
-<button id="export" data-export="export">Export</button>
-</div>
-</div>
+			// The Loop
+			while ( have_posts() ) : the_post();
+				echo '<tr>';
+				echo '<td>'.get_the_ID().'</td>';
+				echo '<td>'.get_the_date('d/m/Y').'</td>';
+				echo '<td>'.get_the_time().'</td>';
+				echo '<td>'.get_post_meta( get_the_ID(),'_billing_first_name', true ).' '.
+					get_post_meta( get_the_ID(),'_billing_last_name', true ).' &#60;'.
+					get_post_meta( get_the_ID(),'_billing_email', true ).'&#62;</td>';
+				foreach($wc_fields_additional as $key=>$values)
+				{
+					echo '<td>'.get_post_meta( get_the_ID(),$key, true ).'</td>';
+				}
+
+				$order_id=get_the_ID();
+				$order = new WC_Order( $order_id );
+				$items = $order->get_items();
+
+				$order_items=array();
+				$order_quantities=array();
+				foreach ( $items as $item ) {
+					$product_name = $item['name'];
+					$product_id = $item['product_id'];
+					$product_variation_id = $item['variation_id'];
+					array_push($order_items,$product_name);
+					array_push($order_quantities,$item['qty']);
+				}
+				echo '<td>'.implode(' , ',$order_items).'</td>';
+				echo '<td>'.implode(' , ',$order_quantities).'</td>';
+				echo '</tr>';
+			endwhile;
+
+			// Reset Query
+			wp_reset_query();
+			?>
+			</tbody>
+		</table>
+		<div>
+			<button id="export" data-export="export">Export</button>
+		</div>
+	</div>
 <?php
 
 function get_all_products_name(){
-		
-		$all_products=array();
-        $args = array( 'post_type' => 'product', 'posts_per_page' => -1,);
-        $loop = new WP_Query( $args );
-        while ( $loop->have_posts() ) : $loop->the_post(); global $product;
-		array_push($all_products,get_the_title());
+
+	$all_products=array();
+	$args = array( 'post_type' => 'product', 'posts_per_page' => -1,);
+	$loop = new WP_Query( $args );
+	while ( $loop->have_posts() ) : $loop->the_post(); global $product;
+		//array_push($all_products,get_the_title());
+		array_push($all_products, html_entity_decode(str_replace("&#8217;","'",get_the_title())) );
 		//echo get_the_title();
-		endwhile;
-		wp_reset_query();
-sort($all_products);
-		return $all_products;
+	endwhile;
+	wp_reset_query();
+	sort($all_products);
+	return $all_products;
 }
 
 function get_all_categories_name(){
-		
-		$all_categories=array();
-		
-		$args = array(
-				'number'     => $number,
-				'orderby'    => 'title',
-				'order'      => 'ASC',
-				'hide_empty' => $hide_empty,
-				'include'    => $ids
-		);
-		$product_categories = get_terms( 'product_cat',$args);
-		$count = count($product_categories);
-		//echo $count;
-		if ( $count > 0 ){
-		    foreach ( $product_categories as $product_category ) {
-				array_push($all_categories,$product_category->name);
-			}
+
+	$all_categories=array();
+
+	$args = array(
+		'number'     => $number,
+		'orderby'    => 'title',
+		'order'      => 'ASC',
+		'hide_empty' => $hide_empty,
+		'include'    => $ids
+	);
+	$product_categories = get_terms( 'product_cat',$args);
+	$count = count($product_categories);
+	//echo $count;
+	if ( $count > 0 ){
+		foreach ( $product_categories as $product_category ) {
+			array_push($all_categories,$product_category->name);
 		}
-		return $all_categories;
+	}
+	return $all_categories;
 }
 
 ?>
